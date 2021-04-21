687530ed34ff4974b0c48c1ba5783eb294e19938
antonin <antoninartaud@hotmail.fr>
2021-04-21 16:52:31 +0200

    Affichage conditionnel ok

--- space-invaders/src/App.js
+++ space-invaders/src/App.js
@@ -1,51 +1,78 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 import Alien from './components/Alien';
 
 class App extends React.Component {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
       alienPositionRow: 1,
-      displayAlien: 'grid'
-    }
+      displayAlien: 'grid',
+      showResultWin: true,
+      showResultLose: false,
+      // showResult: false,
+    };
 
-    this.moveForwardAlien = this.moveForwardAlien.bind(this)
+    this.moveForwardAlien = this.moveForwardAlien.bind(this);
+    this.renderGame = this.renderGame.bind(this);
   }
 
   moveForwardAlien() {
     if (this.state.alienPositionRow <= 43) {
-
       this.setState({ alienPositionRow: this.state.alienPositionRow + 1 });
 
       setTimeout(() => {
-        this.moveForwardAlien()
+        this.moveForwardAlien();
       }, 1000);
-
     } else {
       this.setState({ displayAlien: 'none' });
     }
-
   }
+
+  renderGame() {
+    if (this.state.showResultWin === true) {
+      return <h1>You Win !!</h1>;
+    } else if (this.state.showResultLose === true) {
+      return <h1>You Lose !!</h1>;
+    } else {
+      return (
+        <div
+          style={{
+            overflow: 'hidden',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(11,1fr)',
+            gridTemplateRows: 'repeat(44, 14.2px)',
+            justifyItems: 'center',
+          }}
+        >
+          <Alien
+            myFunc={this.moveForwardAlien}
+            display={this.state.displayAlien}
+            gridPositionColumn={6}
+            gridPositionRow={this.state.alienPositionRow}
+          />
+
+          <Alien
+            myFunc={this.moveForwardAlien}
+            display={this.state.displayAlien}
+            gridPositionColumn={7}
+            gridPositionRow={this.state.alienPositionRow}
+          />
+
+          <Alien
+            myFunc={this.moveForwardAlien}
+            display={this.state.displayAlien}
+            gridPositionColumn={5}
+            gridPositionRow={this.state.alienPositionRow}
+          />
+        </div>
+      );
+    }
+  }
+
   render() {
-    return (
-
-      <div style={{
-        overflow: 'hidden', display: 'grid', gridTemplateColumns: 'repeat(11,1fr)',
-        gridTemplateRows: 'repeat(44, 14.2px)', justifyItems: 'center'
-      }}>
-        <Alien myFunc={this.moveForwardAlien} display={this.state.displayAlien} gridPositionColumn={6}
-          gridPositionRow={this.state.alienPositionRow} />
-
-        <Alien myFunc={this.moveForwardAlien} display={this.state.displayAlien} gridPositionColumn={7}
-          gridPositionRow={this.state.alienPositionRow} />
-
-        <Alien myFunc={this.moveForwardAlien} display={this.state.displayAlien} gridPositionColumn={5}
-          gridPositionRow={this.state.alienPositionRow} />
-      </div>
-    )
+    return <div>{this.renderGame()}</div>;
   }
 }
 
