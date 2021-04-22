6f8bfe1bdc7d51f8162189574f675cda96325ac8
Moncef <moncefboughal@hotmail.fr>
2021-04-22 19:42:23 +0200

    Many aliens

--- space-invaders/src/components/Alien.jsx
+++ space-invaders/src/components/Alien.jsx
@@ -8,8 +8,10 @@ class Alien extends React.Component {
         return (
             <img src={ImgAlien}
                 style={{
-                    display: this.props.display, width: '50px',
-                    height: '50px', gridColumnStart: this.props.gridPositionColumn,
+                    display: "grid",
+                    width: '50px',
+                    height: '50px', 
+                    gridColumnStart: this.props.gridPositionColumn,
                     gridRowStart: this.props.gridPositionRow
                 }}>
             </img>
